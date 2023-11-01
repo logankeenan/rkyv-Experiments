00e282da51ddfccf799e58733614d433cd1d679a
Logan Keenan <d.logankeenan@gmail.com>
2023-10-31 20:14:43 -0500

    add basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get},
+    response::IntoResponse, Router,
+};
+
+
+async fn get_products() -> impl IntoResponse {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(get_products));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
